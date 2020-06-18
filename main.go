500e9b7629743932136777cf7d58fb882358c952
Burak Nehbit <burak@getaether.net>
2020-06-18 15:24:22 -0700

    simplify

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"runtime"
 )
 
 var localAddress string
@@ -23,30 +22,22 @@ func init() {
 
 func main() {
 	flag.Parse()
-
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
 	cert, err := tls.LoadX509KeyPair(certificatePath, keyPath)
 	if err != nil {
 		log.Fatalf("error in tls.LoadX509KeyPair: %s", err)
 	}
-
 	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
-
 	listener, err := tls.Listen("tcp", localAddress, &config)
 	if err != nil {
 		log.Fatalf("error in tls.Listen: %s", err)
 	}
-
 	log.Printf("local server on: %s, backend server on: %s", localAddress, backendAddress)
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("error in listener.Accept: %s", err)
 			break
 		}
-
 		go handle(conn)
 	}
 }
@@ -54,21 +45,18 @@ func main() {
 func handle(clientConn net.Conn) {
 	tlsconn, ok := clientConn.(*tls.Conn)
 	if ok {
-
 		err := tlsconn.Handshake()
 		if err != nil {
 			log.Printf("error in tls.Handshake: %s", err)
 			clientConn.Close()
 			return
 		}
-
 		backendConn, err := net.Dial("tcp", backendAddress)
 		if err != nil {
 			log.Printf("error in net.Dial: %s", err)
 			clientConn.Close()
 			return
 		}
-
 		go Tunnel(clientConn, backendConn)
 		go Tunnel(backendConn, clientConn)
 	}
@@ -77,12 +65,11 @@ func handle(clientConn net.Conn) {
 func Tunnel(from, to io.ReadWriteCloser) {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Printf("recovered while tunneling")
+			// log.Printf("recovered while tunneling")
 		}
 	}()
-
 	io.Copy(from, to)
 	to.Close()
 	from.Close()
-	log.Printf("tunneling is done")
+	// log.Printf("tunneling is done")
 }
